fc6af5352641d243c4d1b2606ef1683f6a8050af
Jan Baraniewski <dev@baraniewski.com>
2024-06-06 11:18:18 +0200

    Fix hashing logic, use external bash provider to calculate sha256

--- single-namespace-rename/main.tf
+++ single-namespace-rename/main.tf
@@ -1,7 +1,23 @@
+data "external" "hash" {
+  program = [
+    "/bin/bash",
+    "-c",
+    <<EOT
+    input="${var.resource_name}-x-${var.namespace}-x-${var.vcluster_name}"
+    hash=$(echo -n "$input" | sha256sum | awk '{print $1}')
+    echo "{\"hash\": \"$hash\"}"
+    EOT
+  ]
+}
+
 locals {
   concatenated_name = "${var.resource_name}-x-${var.namespace}-x-${var.vcluster_name}"
-  hash              = base64encode(sha256(local.concatenated_name))
+  hash              = data.external.hash.result.hash
   digest            = substr(local.hash, 0, 10)
   safe_name         = length(local.concatenated_name) > 63 ? "${substr(local.concatenated_name, 0, 52)}-${local.digest}" : local.concatenated_name
   final_name        = replace(local.safe_name, ".-", "-")
+}
+
+output "safe_concatenated_name" {
+  value = local.final_name
 }
