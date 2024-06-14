1cbe12af27846b29e1882469240db97aba149b76
Jan Baraniewski <dev@baraniewski.com>
2024-06-14 15:04:51 +0200

    Update single-namespace-rename module to use platform API to get synced name

--- single-namespace-rename/main.tf
+++ single-namespace-rename/main.tf
@@ -1,19 +1,33 @@
-data "external" "hash" {
-  program = [
-    "/bin/bash",
-    "-c",
-    <<EOT
-    input="${var.resource_name}-x-${var.namespace}-x-${var.vcluster_name}"
-    hash=$(echo -n "$input" | sha256sum | awk '{print $1}')
-    echo "{\"hash\": \"$hash\"}"
-    EOT
-  ]
+terraform {
+  required_providers {
+    http = {
+       source = "hashicorp/http"
+       configuration_aliases = [
+         http.default,
+       ]
+    }
+  }
+}
+
+data "http" "post_request" {
+  provider = http.default
+  url      = var.url
+  insecure = true
+  request_headers = {
+    "Content-Type"  = "application/json"
+    "Authorization" = "Bearer ${var.auth_token}"
+  }
+
+  request_body = jsonencode({
+    spec = {
+      name         = var.resource_name
+      namespace    = var.resource_namespace
+      vclusterName = var.vcluster_name
+    }
+  })
+  method = "POST"
 }
 
 locals {
-  concatenated_name = "${var.resource_name}-x-${var.namespace}-x-${var.vcluster_name}"
-  hash              = data.external.hash.result.hash
-  digest            = substr(local.hash, 0, 10)
-  safe_name         = length(local.concatenated_name) > 63 ? "${substr(local.concatenated_name, 0, 52)}-${local.digest}" : local.concatenated_name
-  final_name        = replace(local.safe_name, ".-", "-")
+  response_data = jsondecode(data.http.post_request.response_body)
 }
--- single-namespace-rename/outputs.tf
+++ single-namespace-rename/outputs.tf
@@ -1,3 +1,14 @@
 output "name" {
-  value = local.final_name
+  description = "The value of the 'name' key from the 'status' struct in the response of the POST request"
+  value       = local.response_data["status"]["name"]
+}
+
+output "response_code" {
+  description = "The HTTP response code from the POST request"
+  value       = data.http.post_request.status_code
+}
+
+output "response_body" {
+  description = "The full JSON response body from the POST request"
+  value       = data.http.post_request.response_body
 }
